5161578e96c2b8c93a9a7c06b90e57560fdb416a
erdaserhan <erhan209@gmail.com>
2025-02-25 21:59:53 +0100

    MovieProvider function created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,22 @@
-import { Route, Routes } from 'react-router-dom';
-import './css/App.css';
-import Home from './pages/Home';
-import Favorites from './pages/Favorites';
-import Navbar from './components/Navbar';
+import "./css/App.css";
+import Favorites from "./pages/Favorites";
+import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
+import { MovieProvider } from "./contexts/MovieContext";
+import NavBar from "./components/NavBar";
 
 function App() {
-
   return (
-    <div>
-      <Navbar />
-      <main className='main-content'>
+    <MovieProvider>
+      <NavBar />
+      <main className="main-content">
         <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="/favorites" element={<Favorites />}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
         </Routes>
       </main>
-    </div>
-  )
+    </MovieProvider>
+  );
 }
 
-export default App
+export default App;
